aaba3ca2d51d2dd385650096ca49c9e151aca0a5
Peter Širka <petersirka@gmail.com>
2014-12-15 16:04:24 +0100

    new changes

---
+++ public/js/default.js
@@ -0,0 +1,19 @@
+function fullpage() {
+    var $img = $('#fullpage').find('img');
+    if ($img.length === 0)
+        return;
+    var imageWidth = $img[0].width, imageHeight = $img[0].height, maxWidth = $(window).width(), maxHeight = $(window).height(), widthRatio = maxWidth / imageWidth, heightRatio = maxHeight / imageHeight;
+    var ratio = widthRatio;
+
+    if (widthRatio * imageHeight > maxHeight)
+        ratio = heightRatio;
+
+    $img.attr('width', imageWidth * ratio).attr('height', imageHeight * ratio);
+    $('#fullpage').css({ height: imageHeight * ratio });
+}
+
+$(document).ready(function() {
+    $('.menu').find('a[href="' + window.location.pathname + '"]').addClass('selected');
+    $(window).resize(fullpage);
+    fullpage();
+});
